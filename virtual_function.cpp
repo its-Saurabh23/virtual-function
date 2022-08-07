7b6da12f829876cd697a598b3911a091c31c97b9
Mr.Saurabh <kavinelevin12345@gmail.com>
2022-08-07 15:50:09 +0530

    version 2

--- virtual_function.cpp
+++ virtual_function.cpp
@@ -1,34 +1,39 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
+#include <conio.h>
 using namespace std;
-//class Name
-class Base {
-public:
- virtual void disk(){
-    cout<<"\n I am from base class disk function";
-}
-void get(){
-    cout<<"\n I am from base class get function";
-
-}
-};
-
-class drived : virtual public Base 
+// class Name
+class Base
 {
 public:
-void disk(){
-    cout<<"\n I am from drived class disk function ";
-
-}
- void get(){
-    cout<<"\n I am  from drived class get function";
-}
+    virtual void disk()
+    {
+        cout << "\n I am from base class disk function";
+    }
+    void get()
+    {
+        cout << "\n I am from base class get function";
+    }
 };
-int main(){
-Base *p;
-drived objD;
-p=&objD;
-p->disk();
-p->get();
-objD.get();
+
+// Drived Class Name.
+class drived : virtual public Base
+{
+public:
+    void disk()
+    {
+        cout << "\n I am from drived class disk function ";
+    }
+    void get()
+    {
+        cout << "\n I am  from drived class get function";
+    }
+};
+int main()
+{
+    Base *p;
+    drived objD;
+    p = &objD;
+    p->disk();
+    p->get();
+    objD.get();
 }
